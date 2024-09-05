dceda9a35f050bf560715055fdad03cad08d853d
Oyatillohgayratov <azamatgayratov771@gmail.com>
2024-09-05 09:22:06 +0500

    upd: go.mod and go.sum file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Oyatillohgayratov/fitness-tracking-app
 
 go 1.22.2
+
+require github.com/sqlc-dev/pqtype v0.3.0
---
+++ query.sql
@@ -0,0 +1,22 @@
+-- name: GetUser :one 
+select * from users
+where id = $1 limit 1;
+
+-- name: LimitUser :many
+select id, username, email, profile
+from users
+order by username;
+
+-- name: CreateUser :exec
+insert into users (username, password_hash, email, profile)
+values ($1, $2, $3, $4)
+returning *;
+
+-- name: UpdateUser :exec
+update users
+set username = $2, email = $3, profile = $4
+where id = $1;
+
+-- name: DeleteUser :exec
+delete from users
+where id = $1;
